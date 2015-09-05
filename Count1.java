dae3ff11ef2ed50c5c9bb66697f13a776a8175d9
mcolic <mcolic@na.edu>
2015-09-05 01:52:05 -0500

    First commit

--- Count1.java
+++ Count1.java
@@ -2,25 +2,26 @@ import java.util.Scanner;
 import java.io.File;
 import java.util.Hashtable;
 
-class Count
+class Count1
 {
   public static void main(String[] args)
   {
-    try {
+    try { // buna karsi gelen parantez asagida mavi
 	
 	  Hashtable<String,Integer>  wordFreqHashtable =  new Hashtable<String,Integer>();
-	  
+	  Scanner fileReader;
 	  for(int fileID=0; fileID<51; fileID++)
 	  {
 	    File inputFile;
-	    String fileName             = "NonSpamTrain" + fileID + ".txt";
+	    String fileName = "NonSpamTrain" + fileID + ".txt";
 	    System.out.println(fileName);
-		inputFile               = new File(fileName);
-		Scanner fileReader      = new Scanner(inputFile); 
+		inputFile       = new File(fileName);
+		fileReader      = new Scanner(inputFile); 
 		while( fileReader.hasNext() )
 		{
-                  String token = fileReader.next(); // returns the data in the scanner
+            String token = fileReader.next(); // returns the data in the scanner
 
+			tokens.useDelimiter(" ");
 		  if( wordFreqHashtable.containsKey(token.toLowerCase()) )
 		  { // true
 		    wordFreqHashtable.put(token.toLowerCase(), wordFreqHashtable.get(token.toLowerCase())+1);
@@ -30,30 +31,24 @@ class Count
 		    wordFreqHashtable.put(token.toLowerCase(), 1);
 		  }
 		  // how i will scan the elements in the hastable
-		  System.out.println(token);
-}
-
+		  System.out.println(token.next());
+    }
 		  
 		  // scan all the elements in the hastable
 		   
     for (String key : wordFreqHashtable.keySet())
     {
-       if( wordFreqHashtable.get(key) > 25)        
+         if( wordFreqHashtable.get(key) > 25)
+               
      System.out.println(" key: " + key + " value: " + wordFreqHashtable.get(key));
 	
-	
-}
 	}
-               
+}
+	
 
-            }
-	catch(Exception exception)// will catch all the exceptions
+	Catch(Exception exception)
 	{
 	  System.out.println(exception.getMessage());
+	}
+  }// yukariya karsi gelen parantez
 }
-
-  }
-}
-
-
-
